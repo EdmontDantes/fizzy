af6b059c1ca20a8aaacb6f1836cb364a353ad139
Bogdan Kowaltchook <bogdan.kowaltchook@codeimmersives.com>
2020-02-04 14:47:06 -0500

    4 passed , 4 total

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 function fizzy(num) {
-
+  if (num % 3 && !(num % 5) ){
+    return 'Buzz';
+  }
+  else if(num % 5 && !(num % 3)){
+    return 'Fizz';
+  }
+  else if ((num % 5) && (num % 3)){
+  return num;
+        }
+  else if (num % 3 === num % 5){
+    return 'FizzBuzz';
+  }
 }
 
 module.exports = {
